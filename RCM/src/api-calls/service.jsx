56be8c75b713840410f0e1da2157d6019860a49e
Anubhav Rana <anubhav.r@idsil.com>
2024-03-26 20:09:03 +0530

    commit

--- RCM/src/api-calls/service.jsx
+++ RCM/src/api-calls/service.jsx
@@ -2,3 +2,4 @@ import {apiRequest} from "./index.jsx"
 
 export const GetSearchServiceData = async (payload) => apiRequest('post', 'recommendation/',payload);
 export const GetLocationData = async () => apiRequest('get', 'unique-states/');
+export const GetDataByLocation = async () => apiRequest('get', 'exact-match/');
